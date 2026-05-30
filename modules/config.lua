2daf70920a4251440b58e235cd7f575cd3640cf1
Mift <75431345+Mift777@users.noreply.github.com>
2026-05-29 22:17:54 -0300

    a

--- modules/config.lua
+++ modules/config.lua
@@ -65,5 +65,4 @@ return function(env)
     _G.TargetFertilizePlantNames          = {}
     _G.TargetFertilizeMutations           = {}
     _G.TargetFertilizerTypes              = {}
-    _G.SessionUnlockedEggSlots = {}
 end

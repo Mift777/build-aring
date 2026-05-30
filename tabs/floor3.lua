4ea71ba57a643f567e67ff79e62be822f00d7c53
Mift <75431345+Mift777@users.noreply.github.com>
2026-05-30 17:02:27 -0300

    a

--- tabs/floor3.lua
+++ tabs/floor3.lua
@@ -1,7 +1,3 @@
 return function(env)
-    if env.buildFloorTab then
-        env.buildFloorTab(env, 3, env.Floor3Tab)
-    else
-        print("[LamduckHub] floorbuilder not loaded")
-    end
+    if env.buildFloorTab then env.buildFloorTab(env, 3, env.Floor3Tab) end
 end
